3fa00ef79caf80c3fca31b93c8038ee46150b7c8
miguel <tsambamiguelito@gmail.com>
2020-11-17 22:59:41 +0100

    Initial Commit

---
+++ src/main/java/fr/myt/learning/spring/sandbox/SandboxApplication.java
@@ -0,0 +1,13 @@
+package fr.myt.learning.spring.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/myt/learning/spring/sandbox/SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.myt.learning.spring.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
