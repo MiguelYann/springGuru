06dcceaff43df5936ef09220734d2af1037f1c57
miguel <tsambamiguelito@gmail.com>
2020-11-20 02:23:05 +0100

    Bootstrap fake data for see how repository save JPA entity

--- src/main/java/fr/myt/learning/spring/sandbox/SandboxApplication.java
+++ src/main/java/fr/myt/learning/spring/sandbox/SandboxApplication.java
@@ -3,7 +3,6 @@ package fr.myt.learning.spring.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SandboxApplication {
